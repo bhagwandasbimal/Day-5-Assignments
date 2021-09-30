8861da67698ecb24d88723b460499c217043db62
bhagwandasbimal <bhagwandasbimal@gmail.com>
2021-09-30 14:27:21 +0530

    emplDailyWage

---
+++ empWageBuilder.java
@@ -0,0 +1,11 @@
+public class empWageBuilder {
+	public static void main(String[] args) {
+			System.out.println( "Welcome to Employee Wage Computation Program" );
+		int isFullTime= 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime )
+			System.out.println( "Employee is Present" );
+		else
+			System.out.println( "Employee is Absent" );
+			}
+}
