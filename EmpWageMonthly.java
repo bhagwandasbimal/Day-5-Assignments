4ee827bf155bd5214a94d901f1c9a5fb667d1c10
bhagwandasbimal <90851005+bhagwandasbimal@users.noreply.github.com>
2021-10-08 17:14:21 +0530

    Update and rename empWageMonthly.java to EmpWageMonthly.java

--- EmpWageMonthly.java
+++ EmpWageMonthly.java
@@ -1,4 +1,4 @@
-public class empWageMonthly {
+public class EmpWageMonthly {
 	public static void main(String[] args) {
 		System.out.println( "Welcome to Employee Wage Computation Program" );
 		int isFullTime= 1;
