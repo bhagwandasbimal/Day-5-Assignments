bd1936c8e7f9a446864557a9c1c8a475a28c3642
bhagwandasbimal <90851005+bhagwandasbimal@users.noreply.github.com>
2021-10-08 17:13:12 +0530

    Update and rename empWageCase.java to EmpWageCase.java

--- EmpWageCase.java
+++ EmpWageCase.java
@@ -1,4 +1,4 @@
-public class empWageCase {
+public class EmpWageCase {
 	public static void main(String[] args) {
 		System.out.println( "Welcome to Employee Wage Computation Program" );
 		int isFullTime= 1;
