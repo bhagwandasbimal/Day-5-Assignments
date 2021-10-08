54345bbeca61872bd8c1038c0fcab978c98ec13d
bhagwandasbimal <90851005+bhagwandasbimal@users.noreply.github.com>
2021-10-08 17:12:07 +0530

    Update and rename empPartTimeFullTime.java to EmpPartTimeFullTime.java

--- EmpPartTimeFullTime.java
+++ EmpPartTimeFullTime.java
@@ -1,4 +1,4 @@
-public class empPartTimeFullTime {
+public class EmpPartTimeFullTime {
 	public static void main(String[] args) {
 		System.out.println( "Welcome to Employee Wage Computation Program" );
 		int isFullTime= 1;
